76b8fd569342109e0b6daea41e32de0ecc7d0db0
claussfrank <clauss.frank@gmail.com>
2017-03-25 14:35:55 -0600

    Add files via upload

--- ejercicioGrados/ejercicioGrados.js
+++ ejercicioGrados/ejercicioGrados.js
@@ -1,5 +1,13 @@
 function convertirCelsius(){
   var grados= document.getElementById("farenheit").value
-  var resultados = parseInt((grados)-32)*(5/9);
+  var formula = Number(grados-32)/(1.8);
+  var resultados = formula.toFixed(2);
   document.getElementById("resultadoCelcius").value = resultados;
+}
+
+function convertirFahrenheit(){
+    var grados = document.getElementById("celsius").value
+    var formula = Number(grados * 1.8) + 32;
+    var resultados = formula.toFixed(2);
+ document.getElementById("resultadoFahrenheit").value =resultados;
 }
